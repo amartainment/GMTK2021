aef9e946ee5a8ce70fd4ac959e41d4ff56dcffb9
Amar <amar.ravi@outlook.com>
2021-06-13 03:10:34 +0530

    Enemy hover + target

---
+++ Assets/DamagePointManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagePointManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<Transform> points;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Transform ReturnATransform()
+    {
+        Transform targetTransform;
+        int randomIndex = Random.Range(0, points.Count);
+        targetTransform = points[randomIndex];
+        return targetTransform;
+    }
+}
---
+++ Assets/EnemyBehavior.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    DamagePointManager pointManager;
+    Rigidbody2D myRb;
+    Transform target;
+    public float speed = 10;
+    bool hoverTimerRunning = false;
+    public enum State
+    {
+        hover,fly
+    }
+
+    State state;
+
+    void Start()
+    {
+        pointManager = GameObject.Find("DamagePoints").GetComponent<DamagePointManager>();
+        myRb = GetComponent<Rigidbody2D>();
+        state = State.hover;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (state)
+        {
+            case State.hover:
+                
+                Hover();
+                break;
+            case State.fly:
+                FlyToTarget();
+                break;
+
+        }
+    }
+
+
+    void Hover()
+    {
+        if(!hoverTimerRunning)
+        {
+            StartCoroutine("StartFlyTimer");
+        }
+    }
+
+    void FlyToTarget()
+    {
+        
+        Vector3 direction = target.position - transform.position;
+        transform.up = direction.normalized;
+        myRb.velocity = direction.normalized * speed;
+
+    }
+    
+    IEnumerator StartFlyTimer()
+    {
+        hoverTimerRunning = true;
+        yield return new WaitForSeconds(5);
+        target = pointManager.ReturnATransform();
+        state = State.fly;
+        hoverTimerRunning = false;
+    }
+
+
+}
